62f092adb9052ba488f798c5cfc328b014310c4f
sivasai1412 <sivasaianjuri@gmail.com>
2023-11-13 23:34:57 -0600

    Creating signup and about page

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+/***
+ * Creating the controllers for the web pages.
+ */
 @Controller
 public class HomeController {
 
@@ -15,10 +18,19 @@ public class HomeController {
 		return "home";
 	}
 	
+	
 	@RequestMapping("/about")
 	public String about(Model model) {
 		model.addAttribute("title", "About - Smart contact manager");
 		return "about";
 	}
+	
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title", "Register - Smart contact manager");
+		return "signup";
+	}
+	
+	
 	
 }
